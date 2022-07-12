fe46313ac197790b26e14563f770fca367e18dc2
govinda-umd <govindas@umd.edu>
2022-07-12 14:11:20 -0400

    jul22

--- _build/jupyter_execute/nb/jul22/00-data-max_model-isfc_desc-early_vs_late.py
+++ _build/jupyter_execute/nb/jul22/00-data-max_model-isfc_desc-early_vs_late.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# # July 4-6,11, 2022: Compute Intersubject Functional Correlation (ISFC) values for `early` and `late` periods in `threat` and `safe` conditions
+# # July 4-6,11-12, 2022: Compute Intersubject Functional Correlation (ISFC) values for `early` and `late` periods in `threat` and `safe` conditions
 
 # In[1]:
 
@@ -15,7 +15,7 @@ import pandas as pd
 import pickle, random
 from tqdm import tqdm
 from scipy.stats import (norm, zscore, permutation_test)
-from itertools import permutations
+from itertools import combinations
 
 # ISC
 from brainiak.isc import (isc, isfc, bootstrap_isc, compute_summary_statistic, squareform_isfc)
@@ -60,6 +60,8 @@ with open(f"{proj_dir}/data/max/exploratory_data.pkl", 'rb') as f:
     X = pickle.load(f)
 
 
+# ### `early` and `late` periods 
+
 # In[3]:
 
 
@@ -80,27 +82,54 @@ for label, name in zip(args.LABELS, args.NAMES):
         ts[name].append(zscore(x, axis=0))
 
 
-# ## Following the pipeline in ISC tutorial
-# Paper: https://academic.oup.com/scan/article/14/6/667/5489905
-# Tutorial: https://github.com/snastase/isc-tutorial
-# 
-# Statistical significance was assessed by a nonparametric bootstrap hypothesis test resampling left-out subjects and corrected for multiple tests by controlling FDR at .05.
-# 
-# i.e.:
-# 1. Compute leave-one-out (LOO) ISC and ISFC values
-# 2. Bootstrap hypothesis testing, (Chen et al.2016)
-# 3. Multiple tests correction: False Discovery Rate (FDR) at 0.05.
-# 
-# LOO method relates to computing ISFC values, as the mean time series of the remaining subjects converges to the common signal across subjects.
-
-# ## `early` and `late` periods 
-
 # In[4]:
 
 
-def get_isfc_significant_rois(args, ts):
-    corrs = {}; bootstraps = {}; q = {}; z = {}; rois = {}
-    observed_isfcs = {}; observed_p_vals = {}; significant_rois = {}; conf_intervals = {}
+'''
+time series of early and late periods
+'''
+args.PERIODS = ['early', 'late']
+# time periods
+args.TR = 1.25 #seconds
+EARLY = np.arange(2.5, 8.75+args.TR, args.TR) // args.TR
+LATE = np.arange(10.0, 16.25+args.TR, args.TR) // args.TR
+EARLY = EARLY.astype(int)
+LATE = LATE.astype(int)
+args.PERIOD_TRS = [EARLY, LATE]
+
+ts = {}
+for label, name in zip(args.LABELS, args.NAMES):
+    for idx_period, (period, TRs) in enumerate(zip(args.PERIODS, args.PERIOD_TRS)):
+        ts[f"{name}_{period}"] = []
+        for x in X[label]:
+            x = x[:, args.PERIOD_TRS[idx_period], :]
+            trl, t, r = x.shape
+            x = np.reshape(x[:min_trials, ...], (min_trials*t, r))
+            ts[f"{name}_{period}"] += [zscore(x, axis=0)]
+
+
+# ### Intersubject correlations
+# 
+# **Following the pipeline in ISC tutorial**
+# 
+# Paper: https://academic.oup.com/scan/article/14/6/667/5489905
+# Tutorial: https://github.com/snastase/isc-tutorial
+# 
+# Statistical significance was assessed by a nonparametric bootstrap hypothesis test resampling left-out subjects (Leavo-one-out (LOO)). 
+# <!-- and corrected for multiple tests by controlling FDR at .05. -->
+# 
+# i.e.:
+# 1. Compute leave-one-out (LOO) ISC and ISFC values
+# 2. Bootstrap hypothesis testing, (Chen et al.2016: https://doi.org/10.1016/j.neuroimage.2016.05.023)
+# <!-- 3. Multiple tests correction: False Discovery Rate (FDR) at 0.05. -->
+# 
+# LOO method relates to computing ISFC values, as the mean time series of the remaining subjects converges to the common signal across subjects.
+
+# In[5]:
+
+
+def get_isfcs(args, ts):
+    corrs = {}; bootstraps = {}; rois = {}
 
     for block in ts.keys():
         isfcs, iscs = isfc(
@@ -111,8 +140,6 @@ def get_isfc_significant_rois(args, ts):
         corrs[block] = {'isfcs':isfcs, 'iscs':iscs}
 
         bootstraps[block] = {}
-        q[block] = {}
-        z[block] = {}
         rois[block] = {}
         for corr_name in args.CORR_NAMES:
             
@@ -120,19 +147,21 @@ def get_isfc_significant_rois(args, ts):
             observed, ci, p, distribution = bootstrap_isc(
                 corrs[block][corr_name], 
                 pairwise=args.pairwise, 
-                ci_percentile=95, 
                 summary_statistic='median',
-                n_bootstraps=args.n_bootstraps
+                n_bootstraps=args.n_bootstraps,
+                ci_percentile=95, 
+                side='two-sided',
+                random_state=args.SEED
             )
             bootstraps[block][corr_name] = observed, ci, p, distribution
 
-            # # multiple tests correction
-            # q[block][corr_name] = multipletests(bootstraps[block][corr_name][2], method='fdr_by')[1]
-            # z[block][corr_name] = np.abs(norm.ppf(q[block][corr_name]))
-
             # surviving rois
             # rois[block][corr_name] = q[block][corr_name] < 0.05
-            rois[block][corr_name] = bootstraps[block][corr_name][2] < 0.05
+            rois[block][corr_name] = bootstraps[block][corr_name][2] < 0.05 # p < 0.05
+
+            # correlations only for surviving rois
+            # corrs[block][corr_name] *= rois[block][corr_name]
+
             print(
                 (
                     f"percent of significant roi(-pairs) for " 
@@ -141,25 +170,10 @@ def get_isfc_significant_rois(args, ts):
                 )
             )
 
-        # isfc matrix
-        observed_isfcs[block] = squareform_isfc(bootstraps[block]['isfcs'][0], bootstraps[block]['iscs'][0])
-        observed_p_vals[block] = squareform_isfc(bootstraps[block]['isfcs'][2], bootstraps[block]['iscs'][2])
-        significant_rois[block] = squareform_isfc(rois[block]['isfcs'], rois[block]['iscs'])
-        conf_intervals[block] = (
-            squareform_isfc(
-                bootstraps[block]['isfcs'][1][0],
-                bootstraps[block]['iscs'][1][0]
-            ),
-            squareform_isfc(
-                bootstraps[block]['isfcs'][1][1],
-                bootstraps[block]['iscs'][1][1]
-            )
-        ) 
-    
-    return observed_isfcs, observed_p_vals, significant_rois, conf_intervals, corrs, bootstraps, rois
+    return corrs, bootstraps, rois
 
 
-# In[5]:
+# In[6]:
 
 
 def plot_isfcs(args, isfcs, rois): 
@@ -190,42 +204,17 @@ def plot_isfcs(args, isfcs, rois):
             ax = axs[label, idx_period]
             block = f"{name}_{period}"
 
-            im = ax.imshow(isfcs[block] * rois[block], cmap='RdBu_r', vmin=vmin, vmax=vmax)
+            im = ax.imshow(
+                isfcs[block], #* rois[block], 
+                cmap='RdBu_r', vmin=vmin, vmax=vmax
+            )
             ax.figure.colorbar(im, ax=ax, fraction=0.046, pad=0.04)
 
-            ax.set_title(f"{block}")
-            ax.set_ylabel(f"rois")
-            ax.set_xlabel(f"rois")
+            if label == 0: ax.set_title(f"{period}")
+            if idx_period == 0: ax.set_ylabel(f"{name}", size='large')
+            # ax.set_xlabel(f"rois")
     
     return None
-
-
-# ## Intersubject correlations
-
-# In[6]:
-
-
-'''
-time series of early and late periods
-'''
-args.PERIODS = ['early', 'late']
-# time periods
-args.TR = 1.25 #seconds
-EARLY = np.arange(2.5, 8.75+args.TR, args.TR) // args.TR
-LATE = np.arange(10.0, 16.25+args.TR, args.TR) // args.TR
-EARLY = EARLY.astype(int)
-LATE = LATE.astype(int)
-args.PERIOD_TRS = [EARLY, LATE]
-
-ts = {}
-for label, name in zip(args.LABELS, args.NAMES):
-    for idx_period, (period, TRs) in enumerate(zip(args.PERIODS, args.PERIOD_TRS)):
-        ts[f"{name}_{period}"] = []
-        for x in X[label]:
-            x = x[:, args.PERIOD_TRS[idx_period], :]
-            trl, t, r = x.shape
-            x = np.reshape(x[:min_trials, ...], (min_trials*t, r))
-            ts[f"{name}_{period}"] += [zscore(x, axis=0)]
 
 
 # In[7]:
@@ -234,46 +223,146 @@ for label, name in zip(args.LABELS, args.NAMES):
 '''
 leave-one-out ISFC
 bootstrap
-FDR correction
 '''
 args.CORR_NAMES = ['isfcs', 'iscs']
 args.pairwise = False
 args.vectorize_isfcs = True
 args.n_bootstraps = 1000
 
-isfcs, ps, sig_rois, cis, corrs, bootstraps, rois = get_isfc_significant_rois(args, ts)
-plot_isfcs(args, isfcs, sig_rois)
+corrs, bootstraps, rois = get_isfcs(args, ts)
+
+# # multiple tests correction
+# q = {}; z = {};
+# q[block][corr_name] = multipletests(bootstraps[block][corr_name][2], method='fdr_by')[1]
+# z[block][corr_name] = np.abs(norm.ppf(q[block][corr_name]))
 
 
-# Showing ISFC values for each period only for the significant roi-pairs.
-
-# ## statistical testing: pairwise non parametric permutation test
+# Showing ISFC values for each period only for the *significant* roi-pairs.
 
 # In[8]:
 
 
-def statistic(y1, y2, axis):
-    return (
-        compute_summary_statistic(y1, summary_statistic='median', axis=axis) 
-        - compute_summary_statistic(y2, summary_statistic='median', axis=axis)
-    ) 
-
-stats_results = {}; diff_isfcs = {}; diff_pvals = {}
-for (block1, block2) in tqdm(permutations(corrs.keys(), 2)):
-    obs1 = np.concatenate([corrs[block1]['isfcs'], corrs[block1]['iscs']], axis=-1)
-    obs2 = np.concatenate([corrs[block2]['isfcs'], corrs[block2]['iscs']], axis=-1)
-    
-    stats_results[(block1, block2)] = permutation_test(
-        data=(obs1, obs2),
-        statistic=statistic,
-        permutation_type='samples',
-        vectorized=True,
-        n_resamples=args.n_bootstraps,
-        batch=5,
-        axis=0,
-        random_state=args.SEED
+observed_isfcs = {}; observed_p_vals = {}; 
+significant_rois = {}; conf_intervals = {}
+for block in bootstraps.keys():
+    observed_isfcs[block] = squareform_isfc(
+        bootstraps[block]['isfcs'][0], 
+        bootstraps[block]['iscs'][0]
+    )
+    observed_p_vals[block] = squareform_isfc(
+        bootstraps[block]['isfcs'][2], 
+        bootstraps[block]['iscs'][2]
+    )
+    significant_rois[block] = squareform_isfc(
+        rois[block]['isfcs'], 
+        rois[block]['iscs']
+    )
+    conf_intervals[block] = (
+        squareform_isfc(
+            bootstraps[block]['isfcs'][1][0],
+            bootstraps[block]['iscs'][1][0]
+        ),
+        squareform_isfc(
+            bootstraps[block]['isfcs'][1][1],
+            bootstraps[block]['iscs'][1][1]
+        )
     )
 
+    observed_isfcs[block] *= significant_rois[block]
+
+plot_isfcs(args, observed_isfcs, significant_rois)
+
+
+# ### statistical testing: pairwise non parametric permutation test
+# 
+# We perform a non parametric pairwise permutation test suggested by Chen et al.2016 (https://doi.org/10.1016/j.neuroimage.2016.05.023) to compare ISFC matrices between any two pairs of conditions. 
+# 
+# We use statistic in the permutation test as the difference between median ISFC matrices between two conditions.
+# 
+# We plot the statistic between every pair of conditions only for the roi-pairs that differentiate significantly between the conditions.
+
+# In[9]:
+
+
+def get_comparison_stats(args, corrs,):
+    def statistic(obs1, obs2, axis):
+        return (
+            + compute_summary_statistic(obs1, summary_statistic='median', axis=axis) 
+            - compute_summary_statistic(obs2, summary_statistic='median', axis=axis)
+        ) 
+
+    stats_results = {}
+    for (block1, block2) in tqdm(combinations(corrs.keys(), 2)):
+        obs1 = np.concatenate([corrs[block1]['isfcs'], corrs[block1]['iscs']], axis=-1)
+        obs2 = np.concatenate([corrs[block2]['isfcs'], corrs[block2]['iscs']], axis=-1)
+        
+        stats_results[(block1, block2)] = permutation_test(
+            data=(obs1, obs2),
+            statistic=statistic,
+            permutation_type='samples',
+            vectorized=True,
+            n_resamples=args.n_bootstraps,
+            batch=5,
+            alternative='two-sided',
+            axis=0,
+            random_state=args.SEED
+        )
+    
+    return stats_results
+
+
+# In[10]:
+
+
+def plot_isfc_comparisons(args, corrs, diff_isfcs,):
+    vmin, vmax = [], []
+    for block in diff_isfcs.keys():
+        vmin += [np.min(diff_isfcs[block])]
+        vmax += [np.max(diff_isfcs[block])]
+    vmin, vmax = min(vmin), max(vmax)
+
+    nrows, ncols = [len(corrs.keys())]*2
+    fig, axs = plt.subplots(
+        nrows=nrows, 
+        ncols=ncols, 
+        figsize=(4*ncols, 4*nrows), 
+        sharex=False, 
+        sharey=False, 
+        dpi=120
+    )
+
+    plt.subplots_adjust(
+        left=None, bottom=None, 
+        right=None, top=None, 
+        wspace=0.45, hspace=None
+    )
+
+    for idx_blk1, block1 in enumerate(corrs.keys()):
+        for idx_blk2, block2 in enumerate(corrs.keys()):
+
+            if idx_blk1 <= idx_blk2: 
+                axs[idx_blk1, idx_blk2].remove()
+                continue
+
+            ax = axs[idx_blk1, idx_blk2]
+
+            im = ax.imshow(
+                diff_isfcs[(block2, block1)], 
+                cmap='RdBu_r', vmin=vmin, vmax=vmax
+            )
+            ax.figure.colorbar(im, ax=ax, fraction=0.046, pad=0.04)
+
+            if idx_blk1 == idx_blk2+1: ax.set_title(f"{block2}")
+            if idx_blk2 == 0: ax.set_ylabel(f"{block1}", size='large')
+
+
+# In[11]:
+
+
+stats_results = get_comparison_stats(args, corrs)
+
+diff_isfcs = {}; diff_pvals = {}
+for (block1, block2) in stats_results.keys():
     diff_isfcs[(block1, block2)] = squareform_isfc(
         isfcs=stats_results[(block1, block2)].statistic[:-args.num_rois],
         iscs=stats_results[(block1, block2)].statistic[-args.num_rois:]
@@ -282,50 +371,24 @@ for (block1, block2) in tqdm(permutations(corrs.keys(), 2)):
     diff_pvals[(block1, block2)] = squareform_isfc(
         isfcs=stats_results[(block1, block2)].pvalue[:-args.num_rois],
         iscs=stats_results[(block1, block2)].pvalue[-args.num_rois:],
-    ) < 0.05
+    )
+    diff_pvals[(block1, block2)] = diff_pvals[(block1, block2)] < 0.05
+    # FDR correction
+    # coming soon...
 
+    # keep isfc values only for significant roi pairs
     diff_isfcs[(block1, block2)] *= diff_pvals[(block1, block2)]
+    diff_isfcs[(block1, block2)] *= significant_rois[block1]
+    diff_isfcs[(block1, block2)] *= significant_rois[block2]
 
 
-# In[44]:
+cond_idx = {}
+for idx, block in enumerate(corrs.keys()):
+    cond_idx[block] = idx
 
 
-vmin, vmax = [], []
-for block in diff_isfcs.keys():
-    vmin += [np.min(diff_isfcs[block])]
-    vmax += [np.max(diff_isfcs[block])]
-vmin, vmax = min(vmin), max(vmax)
+# In[12]:
 
-nrows, ncols = [len(args.LABELS) * len(args.PERIODS)]*2
-fig, axs = plt.subplots(
-    nrows=nrows, 
-    ncols=ncols, 
-    figsize=(4*ncols, 4*nrows), 
-    sharex=False, 
-    sharey=False, 
-    dpi=120
-)
 
-plt.subplots_adjust(
-    left=None, bottom=None, 
-    right=None, top=None, 
-    wspace=0.5, hspace=0.25
-)
-
-blocks = np.stack(list(diff_isfcs.keys()))
-for idx_blk1, block1 in enumerate(corrs.keys()):
-    for idx_blk2, block2 in enumerate(corrs.keys()):
-        if idx_blk1 <= idx_blk2: 
-            axs[idx_blk1, idx_blk2].remove()
-            continue
-
-        ax = axs[idx_blk1, idx_blk2]
-
-        im = ax.imshow(diff_isfcs[(block1, block2)], cmap='RdBu_r', vmin=vmin, vmax=vmax)
-        ax.figure.colorbar(im, ax=ax, fraction=0.046, pad=0.04)
-
-        if idx_blk1 == idx_blk2+1: ax.set_title(f"{block2}")
-        if idx_blk2 == 0: ax.set_ylabel(f"{block1}", size='large')
-        # ax.set_xlabel(f"rois")
-    
+plot_isfc_comparisons(args, corrs, diff_isfcs)
 
